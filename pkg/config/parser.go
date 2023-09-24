bfab2bfaa558c719ac0346a27da4f7da639ac0a5
Julie Vogelman <julievogelman0@gmail.com>
2023-09-24 15:26:03 -0700

    remove unused code
    Signed-off-by: Julie Vogelman <julievogelman0@gmail.com>

--- pkg/config/parser.go
+++ pkg/config/parser.go
@@ -1,8 +1,6 @@
 package config
 
 import (
-	"encoding/json"
-
 	"gopkg.in/yaml.v2"
 )
 
@@ -29,25 +27,6 @@ func (p *YAMLConfigParser) Parse(configString string) (*Config, error) {
 
 func (p *YAMLConfigParser) UnParse(config *Config) (string, error) {
 	b, err := yaml.Marshal(config)
-	if err != nil {
-		return "", err
-	}
-	return string(b), nil
-}
-
-type JSONConfigParser struct{}
-
-func (p *JSONConfigParser) Parse(configString string) (*Config, error) {
-	c := &Config{}
-	err := json.Unmarshal([]byte(configString), c)
-	if err != nil {
-		return nil, err
-	}
-	return c, nil
-}
-
-func (p *JSONConfigParser) UnParse(config *Config) (string, error) {
-	b, err := json.Marshal(config)
 	if err != nil {
 		return "", err
 	}
